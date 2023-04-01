ab0c2b6937760123490c94fefb1a29cc5793aefa
roman_awale <roman@soanitech.com>
2023-04-02 00:55:26 +0545

    minor change

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -96,7 +96,6 @@
         @yield('content')
 
     </main><!-- End #main -->
-    {{-- @include('layouts.backend.inc.admin-footer') --}}
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
 
